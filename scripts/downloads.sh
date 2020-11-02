575b884098c3b33bbeae611cc23be58cb7da312e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-11-02 13:47:58 -0500

    supplies

---
+++ scripts/downloads.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget https://gist.githubusercontent.com/bmschmidt/2f5d3cae74c0c869fafc/raw/588b5c6f73f66efa703937fd2dd092b1c372e802/d3.v3.min.js
+wget https://gist.githubusercontent.com/mbostock/4062045/raw/35f1541d6e814e51eb4b6ddf4d235b4d39012fab/miserables.json
+wget https://raw.githubusercontent.com/googlearchive/code-prettify/master/styles/desert.css
+wget https://raw.githubusercontent.com/googlearchive/code-prettify/master/src/prettify.css
+wget https://raw.githubusercontent.com/googlearchive/code-prettify/master/src/prettify.js
+wget https://raw.githubusercontent.com/d3/d3-sankey/master/src/sankey.js
+wget http://d3js.org/topojson.v1.min.js
+wget https://github.com/sparql-book/sparql-book/blob/master/chapter5/D3/japan.topojson
+
+# TODO: topojson-1.5.3.tar.gz included.
+# https://github.com/mbostock/topojson/blob/master/examples/world-50m.json
+# https://getbootstrap.com/docs/4.4/getting-started/download/
